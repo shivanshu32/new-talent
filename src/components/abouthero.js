cb8196bc43c199a90a2a48fdc9887bea0e32bd02
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2024-05-11 08:51:08 +0530

    save

--- src/components/abouthero.js
+++ src/components/abouthero.js
@@ -1,21 +1,32 @@
 export default function Abouthero ()  {
     return (
         <>
-       <div class="relative  from-gray-100 to-gray-500 h-[70vh] text-white overflow-hidden">
-  <div class="absolute inset-0">
+       <div class="relative  from-gray-100 to-gray-500 h-[70vh] sm:h-[30vh] text-white overflow-hidden">
+  <div class="absolute inset-0 hidden sm:block">
     <img src="/pic16.png" alt="Background" class="object-cover ml-16 object-center" />
     <div class="absolute inset-0 bg-black opacity-20"></div>
   </div>
+
+  {/* <div class="block md:hidden">
+    <img src="/pic16.png" alt="Background" class="object-cover  object-center" />
+    <div class="absolute inset-0 bg-black opacity-20"></div>
+  </div> */}
   
 
-<div class="absolute top-1/4 mr-8 right-0 w-1/2">
-<h1  className="lg:text-4xl text-3xl font-bold  leading-tight text-gray-800">
+<div class="absolute top-1/4 lg:mr-8 right-0 sm:w-full lg:w-1/2">
+<h1  className="lg:text-4xl text-3xl font-bold  leading-tight text-gray-800 hidden sm:block">
                             New Talent Models Grooming School
                         </h1>
-                        <p role="contentinfo" className="mt-4 pr-8 text-base leading-6 text-gray-600">
+                        <p role="contentinfo" className="mt-4 pr-8 text-base leading-6 text-gray-600 hidden sm:block ">
                  
 At the New Talent Models Grooming School, we specialize in sculpting the stars of tomorrow. Our innovative curriculum blends traditional modeling techniques with cutting-edge industry insights, equipping aspiring models with the skills and confidence to shine on the runway and beyond. Led by seasoned professionals and fashion veterans, our program emphasizes not only the physical aspects of modeling but also the importance of professionalism, resilience, and self-expression. From mastering the perfect runway strut to cultivating a unique personal brand, we empower our students to navigate the competitive world of fashion with grace and authenticity. Whether they dream of gracing magazine covers, strutting down catwalks, or starring in advertising campaigns, our graduates emerge ready to conquer the industry and leave their mark on the world of fashion.
                         </p>
+
+
+                        <p role="contentinfo" className="mt-4 p-2  text-base leading-6 bg-black opacity-20 text-white">
+                 
+                 At the New Talent Models Grooming School, we specialize in sculpting the stars of tomorrow. Our innovative curriculum blends traditional modeling techniques with cutting-edge industry insights, equipping aspiring models with the skills and confidence to shine on the runway and beyond. 
+                                         </p>
                         
 
 </div>
